ea7cac42f100fb325e88f6d587898c7d31d36ba9
Alon <downtown2u@gmail.com>
2019-06-25 03:09:58 +0300

    Added fonts and widgets to BetterTkinter

--- Assets/BetterTkinter.py
+++ Assets/BetterTkinter.py
@@ -1,9 +1,29 @@
-import tkinter as tk
+import tkinter
+from tkinter import ttk, font
 
 
-class Tk(tk.Tk):
+class DefaultFont(font.Font):
     pass
 
 
-class Button(tk.Button):
+class RegularFont(DefaultFont):
+    def __init__(self):
+        DefaultFont.__init__(self, size=12)
+
+
+class BigFont(DefaultFont):
+    def __init__(self):
+        DefaultFont.__init__(self, size=16)
+
+
+class Tk(tkinter.Tk):
     pass
+
+
+class Button(ttk.Button):
+    pass
+
+
+class Entry(ttk.Entry):
+    def __init__(self, *args, **kwargs):
+        ttk.Entry.__init__(self, font=BigFont(), *args, **kwargs)
