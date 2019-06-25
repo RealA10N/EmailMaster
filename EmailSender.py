8178a0249a42ac0e92ca07b497424a8cd0d6bd8b
Alon <downtown2u@gmail.com>
2019-06-25 03:15:03 +0300

    Added basic entry to main window

--- EmailSender.py
+++ EmailSender.py
@@ -1,1 +1,21 @@
 import Assets.BetterTkinter as tk
+
+
+class Window(tk.Tk):
+
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.title("EmailSender")
+
+        self.LoadWidgets()
+
+    def LoadWidgets(self):
+
+        SenderEmail = tk.Entry(self)
+        SenderEmail.grid()
+
+
+if __name__ == "__main__":
+    root = Window()
+    root.mainloop()
