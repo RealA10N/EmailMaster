3895d25776743654e6ad6ba75c2b108e404e6035
Alon <downtown2u@gmail.com>
2019-06-26 15:09:11 +0300

    Added NameEntry class frame to GUI

--- EmailSender.py
+++ EmailSender.py
@@ -19,6 +19,24 @@ class TitleFrame(tk.Frame):
         tk.SmallLabel(self, text='By RealA10N').BasicGrid()
 
 
+class NameEntry(tk.Frame):
+
+    def __init__(self, Name, DefaultEntry='', *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+
+        self._value = tk.StringVar()
+        self.SetValue(DefaultEntry)
+
+        tk.SmallLabel(self, text='{}:'.format(Name)).BasicGrid(sticky='w')
+        tk.RegularEntry(self, textvariable=self._value).grid()
+
+    def GetValue(self):
+        return self._value.get()
+
+    def SetValue(self, string):
+        self._value.set(string)
+
+
 if __name__ == "__main__":
     root = Window()
     root.mainloop()
