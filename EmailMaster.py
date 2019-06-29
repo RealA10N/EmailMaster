aaea529d6f5a8bd3271ebc65aa1c6693c4d1b8b0
Alon <downtown2u@gmail.com>
2019-06-29 23:48:28 +0300

    Created EmailMaster.py

---
+++ EmailMaster.py
@@ -0,0 +1,13 @@
+from SenderGUI import Window
+
+
+class EmailMaster():
+
+    def __init__(self):
+
+        self._Window = Window()
+        self._Window.mainloop()
+
+
+if __name__ == "__main__":
+    EmailMaster()
