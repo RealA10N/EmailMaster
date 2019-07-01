28d263321809d51121a6fdbef23aa941472a5509
Alon <downtown2u@gmail.com>
2019-07-02 00:19:31 +0300

    Added GrapicPopup class

--- EmailMaster.py
+++ EmailMaster.py
@@ -1,4 +1,5 @@
 from SenderGUI import Window
+from tkinter import messagebox
 
 
 class EmailMaster():
@@ -7,6 +8,20 @@ class EmailMaster():
 
         self._Window = Window()
         self._Window.mainloop()
+
+class GrapicPopup():
+
+    def __init__(self, Title, Desc):
+        self._TitleDescArgs = [Title, Desc]
+
+    def ShowError(self):
+        messagebox.showerror(*self._TitleDescArgs)
+
+    def ShowWarning(self):
+        messagebox.showwarning(*self._TitleDescArgs)
+
+    def ShowInfo(self):
+        messagebox.showinfo(*self._TitleDescArgs)
 
 
 if __name__ == "__main__":
